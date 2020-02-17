1d9f8faebd59b8bb624c8336e2e353e36047c60a
Megan Horsfield <meganthadev@gmail.com>
2020-02-17 07:47:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,7 @@ def oxford_comma(array)
   if array.size == 1 
     return array[0]
   elsif array.size == 2 
-  return array.join
-    elsif array 
+    return array.join
+  else 
+    return array 
 end
